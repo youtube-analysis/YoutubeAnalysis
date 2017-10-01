8b7bf56a68ef988ff23bd2357fada9f429099f8a
kurotkin <kurotkin@users.noreply.github.com>
2017-10-01 21:16:29 +0300

    Добавлен Maven

--- src/main/java/com/github/youtube_analysis/Main.java
+++ src/main/java/com/github/youtube_analysis/Main.java
@@ -1,25 +1,34 @@
 package com.github.youtube_analysis;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class Main {
+public class Main extends Application {
     private static String key;
 
     public static void main(String[] args) {
-        loadKey();
-
+        launch(args);
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        loadKey();
+    }
+
+    // Загрузка ключа API из файла
     private static void loadKey(){
         try {
             FileReader fileReader = new FileReader("key.txt");
             BufferedReader reader = new BufferedReader(fileReader);
             key = reader.readLine();
         } catch (FileNotFoundException e) {
-            System.out.println("Нет файла key.txt");
+            String errorMessage = "Нет файла key.txt";
+            System.out.println(errorMessage);
             System.exit(0);
         } catch (IOException e) {
             e.printStackTrace();
