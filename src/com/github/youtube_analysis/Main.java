03901b40ba17d529b9ca91f5c883c01d6b3a8005
kurotkin <kurotkin@users.noreply.github.com>
2017-10-01 20:56:00 +0300

    Реализована функция загрузки ключа

--- src/com/github/youtube_analysis/Main.java
+++ src/com/github/youtube_analysis/Main.java
@@ -1,8 +1,28 @@
 package com.github.youtube_analysis;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
+    private static String key;
 
     public static void main(String[] args) {
-	// write your code here
+        loadKey();
+
+    }
+
+    private static void loadKey(){
+        try {
+            FileReader fileReader = new FileReader("key.txt");
+            BufferedReader reader = new BufferedReader(fileReader);
+            key = reader.readLine();
+        } catch (FileNotFoundException e) {
+            System.out.println("Нет файла key.txt");
+            System.exit(0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
